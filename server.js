c6b84cda8eaeb3533eb733216686bcc53f141ec3
InktopiaDesigns <inktopiadesigns@gmail.com>
2025-05-24 17:32:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-  imagekitId: process.env.IMAGEKIT_ID, // 🔥 This is required!
 });
 
 
