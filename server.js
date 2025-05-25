d9ea9ff6085006a78ba610eea4036f32492e7bd1
InktopiaDesigns <inktopiadesigns@gmail.com>
2025-05-24 17:36:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const ImageKit = require("imagekit");
 
 const app = express();
 
+console.log("PUBLIC:", process.env.IMAGEKIT_PUBLIC_KEY);
+console.log("PRIVATE:", process.env.IMAGEKIT_PRIVATE_KEY);
+console.log("ENDPOINT:", process.env.IMAGEKIT_URL_ENDPOINT);
+
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
